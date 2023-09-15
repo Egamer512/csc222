363376df06c18f914317efa99b3b002df3d7f420
Anfal Hosen <Egamer8544@gmail.com>
2023-09-15 13:15:35 +0000

    New Commmit

---
+++ Exercises/ch02/ten_expressions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int x = 2; int y=6;
+    cout << "The sum of " << x << " and " << y << " is " << x+y << "." << endl;
+    cout << "The difference of " << x << " and " << y << " is " << x-y << "." << endl;
+    cout << "The division of " << y << " and " << x << " is " << y/x << "." << endl;
+    cout << "The multiplication of " << x << " and " << y << " is " << x*y << "." << endl;
+
+
+    int j = 53; int k = 13;
+   
+  
+
+    cout << "The sum of " << j << " and " << k << " is " << j+k << "." << endl;
+    cout << "The difference of " << j << " and " << k << " is " << j-k << "." << endl;
+    cout << "The division of " << j << " and " << k << " is " << j/k << "." << endl;
+    cout << "The multiplication of " << j << " and " << k << " is " << j*k << "." << endl;
+
+
+    cout << "The sum of " << j << ", " << k << ", " << x << ", and " << y << " is " << j+k+x+y << "." << endl;
+    cout << "The difference of " << j << ", " << k << ", " << x << ", and " << y << " is " << j-k-x-y << "." << endl;
+
+
+    return 0;
+}
--- Notes/things2remember.md
+++ Notes/things2remember.md
@@ -4,3 +4,9 @@
    ```
    % ssh ahosen@students.gctaa.net
    ```
+
+## Using git
+
+1. ``git status`` gives me the "status".
+2. Then the dance - ``git add .``, ``git commit -m "My message"``, and
+   ``git push``.
