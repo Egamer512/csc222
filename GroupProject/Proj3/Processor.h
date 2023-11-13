5646704f8c0f29f1d93f9cbe1ddde670a75ab8b4
Anfal Hosen <Egamer8544@gmail.com>
2023-11-13 12:56:13 +0000

    Working with doctest

--- GroupProject/Proj3/Processor.h
+++ GroupProject/Proj3/Processor.h
@@ -3,20 +3,12 @@
 
 using namespace std;
 
-struct Processor{
-
-    int core_count;
-    int thread_count;
-    double clock_speed;
-    int process;
+struct Processor {
     string brand;
     string gen;
 
     Processor();
-    Processor(int cc, int tc, double cs, int p, string b, string g);  
     
     string to_string();
-    bool equals();  
-
-}
+};
 
--- GroupProject/Proj3/test_Processor.cpp
+++ GroupProject/Proj3/test_Processor.cpp
@@ -4,8 +4,8 @@
 #include "Processor.h"
 using namespace std;
 
-TEST_CASE("Test the to_string of the struct"){
-    Processor intel5();
+TEST_CASE("Test the to_string of the struct") {
+    Processor intel5;
     CHECK(intel5.to_string() == "intel 5th gen");
 }
 
