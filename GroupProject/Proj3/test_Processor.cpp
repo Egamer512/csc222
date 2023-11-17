0266c02ac7b901e3041c13fbc2de32d9daf6ef09
Anfal Hosen <Egamer8544@gmail.com>
2023-11-17 02:05:43 +0000

    the overflow case of the add function works

--- GroupProject/Proj3/test_Processor.cpp
+++ GroupProject/Proj3/test_Processor.cpp
@@ -19,3 +19,15 @@ TEST_CASE("Test the XOR function"){
     intel5.XOR(bitset<8>("11101001"));
     CHECK(intel5.to_string() == "Intel i5-5200U with 01000011 bits");
 }
+
+TEST_CASE("Test the AND function"){
+    Processor intel5;
+    intel5.AND(bitset<8>("11101001"));
+    CHECK(intel5.to_string() == "Intel i5-5200U with 10101000 bits");
+}
+
+TEST_CASE("Test the add function"){
+    Processor intel5;
+    intel5.add(bitset<8>("11010100"));
+    CHECK(intel5.to_string() == "Intel i5-5200U with 00000000 bits");
+}
